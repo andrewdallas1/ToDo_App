14d84b3df452617998542231b9d4bda3cf41a2d8
andrew dallas <adal1@live.com>
2017-01-12 15:22:52 -0500

    User can create todo

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2Wn0O5Gvhtr7iGqv7VcLqyZ1eXwNBNPU",
+    authDomain: "todo-28af4.firebaseapp.com",
+    databaseURL: "https://todo-28af4.firebaseio.com",
+    storageBucket: "todo-28af4.appspot.com",
+    messagingSenderId: "778876409891"
+  };
+  firebase.initializeApp(config);
